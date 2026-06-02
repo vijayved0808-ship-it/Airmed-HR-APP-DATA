7210322a23ca0329b110c464e17a3c574c9afadf
vijayved0808-ship-it <vijayved0808@gmail.com>
2026-06-02 11:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB Connected")).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 // Helper: Distance Calculator (Haversine)
 function getDistance(lat1, lon1, lat2, lon2) {
