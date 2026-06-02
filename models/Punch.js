fac74517b79279431d6bd5e405cc44b542856c22
vijayved0808-ship-it <vijayved0808@gmail.com>
2026-06-02 14:16:40 +0530

    Update Punch.js

--- models/Punch.js
+++ models/Punch.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 
 const punchSchema = new mongoose.Schema({
     empId: { type: String, required: true },
-    date: { type: String, required: true }, // YYYY-MM-DD
+    date: { type: String, required: true }, 
     status: { type: String, default: 'Absent' },
     errorLogs: { type: String, default: null },
     remark: { type: String, default: null },
     punches: [{
         type: { type: String, enum: ['IN', 'OUT'] },
-        time: { type: String }, // HH:MM:SS
+        time: { type: String }, 
         lat: { type: Number },
         lng: { type: Number },
         accuracy: { type: Number },
+        location: { type: String }, // <-- NAYA: Location ka naam save hoga
         remark: { type: String, default: null }
     }]
 }, { timestamps: true });
